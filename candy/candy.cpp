24547f3f1277a55f42984b11f12883ee2173d196
Deepak Kumar <robinsdeepak@gmail.com>
2021-12-28 22:51:48 +0530

    Time: 20 ms (72.09%), Space: 17.8 MB (62.62%) - LeetHub

---
+++ candy/candy.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    int candy(vector<int>& arr) 
+    {
+        int n = arr.size();
+        vector<int> candies(n, 1);
+        
+        for (int i=1; i<n; i++)
+        {
+            if (arr[i] > arr[i-1])
+            {
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+        
+        for (int i=n-2; i>=0; i--)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                candies[i] = max(candies[i+1] + 1, candies[i]);
+            }
+        }
+        
+        int s = 0;
+        for (auto x: candies)
+        {
+            s += x;
+        }
+        return s;
+    }
+};
