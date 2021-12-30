22bb834448c7be34b4926df56dc629cd8415d9c7
Deepak Kumar <robinsdeepak@gmail.com>
2021-12-30 07:36:22 +0530

    Time: 96 ms (28.38%), Space: 48.2 MB (78.43%) - LeetHub

---
+++ jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    bool canJump(vector<int>& nums) 
+    {
+        int reach = 0;
+        
+        for (int i = 0; i < nums.size() && i<=reach; i++)
+        {
+            reach = max(reach, i + nums[i]);
+            if (reach >= nums.size() - 1)
+                return true;
+        }
+        return false;
+    }
+};
+
