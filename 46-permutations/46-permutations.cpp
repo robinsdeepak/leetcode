b654cbcab2086a43ac4a8ec6d59a6077bbcbe179
Deepak Kumar <robinsdeepak@gmail.com>
2022-01-14 19:33:38 +0530

    Time: 12 ms (8.44%), Space: 9.5 MB (5.80%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     
-    bool find(vector<int> v, int x){
+    bool find(vector<int> v, int x)
+    {
         for (int i: v)
-        {
             if (i == x)
                 return true;
-        }
         return false;
     }
+    
     void backtrack(vector<int> v, vector<vector<int>> &vv, vector<int>& nums)
     {
         if (v.size() == nums.size())
@@ -21,11 +21,9 @@ public:
         {
             if (!find(v, x))
             {
-                // indices.insert(x);
                 v.push_back(x);
                 backtrack(v, vv, nums);
                 v.pop_back();
-                // indices.erase(indices.find(x));
             }
         }
     }
@@ -34,7 +32,6 @@ public:
     {
         vector<int> v;
         vector<vector<int>> vv;
-        unordered_set<int> indices;
         
         backtrack(v, vv, nums);
         return vv;
