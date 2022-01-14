fe56a70eaafa39ba33318f903ed0c752634a5f5d
Deepak Kumar <robinsdeepak@gmail.com>
2022-01-15 00:13:17 +0530

    Time: 223 ms (19.43%), Space: 61.7 MB (80.88%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -5,8 +5,7 @@ public:
         int n = arr.size();
         int max_area = -1;
         stack<int> st;
-        int i=0;
-        for (; i<n; i++)
+        for (int i=0; i<n; i++)
         {
             while (!st.empty() && arr[st.top()] >= arr[i])
             {
@@ -20,7 +19,7 @@ public:
         {
             int tp = st.top();
             st.pop();
-            max_area = max((arr[tp] * (st.empty() ? i : i - st.top() - 1)), max_area);
+            max_area = max((arr[tp] * (st.empty() ? n :  n - st.top() - 1)), max_area);
         }
         return max_area;
     }
