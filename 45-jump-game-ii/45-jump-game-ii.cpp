3c49aefc99755a6d52d0b4dcbfd813cb6e9e1380
Deepak Kumar <robinsdeepak@gmail.com>
2022-01-15 11:50:59 +0530

    Time: 1165 ms (5.01%), Space: 17.8 MB (6.53%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -5,20 +5,19 @@ public:
         if (index >= nums.size()-1)
             return 0;
         
-        if (dp[index] != -1)
+        if (dp[index] != nums.size() + 1)
             return dp[index];
         
-        int min_jump = nums.size() + 1;
         for (int i=1; i<=nums[index]; i++)
         {
-            min_jump = min(min_jump, 1 + solve(dp, nums, index + i));
+            dp[index] = min(dp[index], 1 + solve(dp, nums, index + i));
         }
-        return dp[index] = min_jump;
+        return dp[index];
     }
     
     int jump(vector<int>& nums) 
     {
-        vector<int> dp(nums.size(), -1);
+        vector<int> dp(nums.size(), nums.size() + 1);
         return solve(dp, nums, 0);
     }
 };
