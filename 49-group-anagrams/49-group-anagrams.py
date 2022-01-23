ed2d4684ad8dc589b03466f680c41c47e2aaac01
Deepak Kumar <robinsdeepak@gmail.com>
2022-01-23 14:04:48 +0530

    Time: 133 ms (41.98%), Space: 17.3 MB (79.18%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,13 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        strs2 = sorted([("".join(sorted(strs[i])), i) for i in range(len(strs))], key=lambda x: x[0])
-        # print(strs2)
-        group = [[strs[strs2[0][1]]]]
-        
-        for i in range(1, len(strs)):
-            if (strs2[i][0] != strs2[i - 1][0]):
-                group.append([])
-            group[-1].append(strs[strs2[i][1]])
-
-        return group
+        str_dict = {}
+        for s in strs:
+            ss = "".join(sorted(s))
+            if ss not in str_dict:
+                str_dict[ss] = []
+            str_dict[ss].append(s)
+        return list(str_dict.values())
+    
+    
     
