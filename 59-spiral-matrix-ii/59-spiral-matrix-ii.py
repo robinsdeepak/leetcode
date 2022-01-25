9e0b6dc73e4556c016f3b25e7a7790fa83603970
Deepak Kumar <robinsdeepak@gmail.com>
2022-01-25 19:28:05 +0530

    Time: 46 ms (32.29%), Space: 14.3 MB (75.49%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.py
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.py
@@ -6,14 +6,12 @@ class Solution:
         cbegin, cend = 0, n - 1
         k = 1
         while (rbegin <= rend and cbegin <= cend):
-            # arr.extend([matrix[rbegin][col] for col in range(cbegin, cend + 1)])
             for col in range(cbegin, cend + 1):
                 matrix[rbegin][col] = k
                 k += 1
                 
             rbegin += 1
             
-            # arr.extend([matrix[row][cend] for row in range(rbegin, rend + 1)])
             for row in range(rbegin, rend + 1):
                 matrix[row][cend] = k
                 k += 1
@@ -21,14 +19,12 @@ class Solution:
             cend -= 1
             
             if (rbegin <= rend):
-                # arr.extend([matrix[rend][col] for col in range(cend, cbegin - 1, -1)])
                 for col in range(cend, cbegin - 1, -1):
                     matrix[rend][col] = k
                     k += 1
             rend -= 1
             
             if (cbegin <= cend):
-                # arr.extend([matrix[row][cbegin] for row in range(rend, rbegin - 1, -1)])
                 for row in range(rend, rbegin - 1, -1):
                     matrix[row][cbegin] = k
                     k += 1
