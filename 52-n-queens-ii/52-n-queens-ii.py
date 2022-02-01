e6a3cdc05eb5770024c5c14a106e7aedec1a4949
Deepak Kumar <robinsdeepak@gmail.com>
2022-02-01 23:53:23 +0530

    Delete 52-n-queens-ii.py

--- 52-n-queens-ii/52-n-queens-ii.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    
-    def totalNQueens(self, n: int) -> int:
-        # cheat
-        m = {1: 1, 2: 0, 3: 0, 4: 2, 5: 10, 6: 4, 7: 40, 8: 92, 9: 352}
-        return m[n]
-    
