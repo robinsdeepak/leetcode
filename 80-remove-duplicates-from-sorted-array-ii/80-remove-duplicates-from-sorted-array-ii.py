0ea86c8823e7ce3ec76bbff1b683a8cc1b5851ec
Deepak Kumar <robinsdeepak@gmail.com>
2022-02-06 16:40:56 +0530

    Time: 50 ms (48.83%), Space: 13.5 MB (57.23%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
@@ -4,9 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        i = 0
-        for n in nums:
-            if i < 2 or n > nums[i-2]:
-                nums[i] = n
-                i += 1
-        return i
+        j = 2
+        
+        for i in range(2, len(nums)):
+            if (nums[i] != nums[j-2]):
+                nums[j] = nums[i]
+                j += 1
+        return j
+        
