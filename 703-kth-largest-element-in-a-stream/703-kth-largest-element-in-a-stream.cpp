edacb575f8a5bd2c3abb6435b2223abdc9c49a6a
Deepak Kumar <robinsdeepak@gmail.com>
2022-02-27 19:19:31 +0530

    Time: 65 ms (26.47%), Space: 24 MB (5.38%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.cpp
@@ -16,16 +16,9 @@ public:
         
         if (s1.size() > K) {
             int last = *s1.rbegin();
-            // multiset<int>::iterator it = s1.rbegin();
             s1.erase(prev(s1.end()));
             s2.insert(last);
         }
-        
-        // cout << K <<" " << s1.size() << " " << s2.size() << endl;
-        // for (int x: s1) cout << x << " ";
-        // cout << " | ";
-        // for (int x: s2) cout << x << " ";
-        // cout << endl;
         
         return (*s1.rbegin());
     }
