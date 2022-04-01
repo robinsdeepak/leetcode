600343b3dc8662204dff3fedb1ef8faf78c17b8c
Deepak Kumar <robinsdeepak@gmail.com>
2022-04-01 10:01:48 +0530

    Time: 3750 ms (16.58%), Space: 22.4 MB (9.38%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.py
+++ 410-split-array-largest-sum/410-split-array-largest-sum.py
@@ -1,34 +1,23 @@
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
         n = len(nums)
-        
-        # Create a prefix sum array of nums.
-        prefix_sum = [0] + list(itertools.accumulate(nums))
+        prefix = [0]
+        for x in nums: prefix.append(prefix[-1] + x)
         
         @functools.lru_cache(None)
-        def get_min_largest_split_sum(curr_index: int, subarray_count: int):
-            # Base Case: If there is only one subarray left, then all of the remaining numbers
-            # must go in the current subarray. So return the sum of the remaining numbers.
-            if subarray_count == 1:
-                return prefix_sum[n] - prefix_sum[curr_index]
+        def solve(idx, scount):
+            if scount == 1:
+                return prefix[-1] - prefix[idx]
         
-            # Otherwise, use the recurrence relation to determine the minimum largest subarray sum
-            # between curr_index and the end of the array with subarray_count subarrays remaining.
-            minimum_largest_split_sum = prefix_sum[n]
-            for i in range(curr_index, n - subarray_count + 1):
-                # Store the sum of the first subarray.
-                first_split_sum = prefix_sum[i + 1] - prefix_sum[curr_index]
+            mlss = prefix[-1]
+            for i in range(idx, n - scount + 1):
+                first_split_sum = prefix[i + 1] - prefix[idx]
+                largest_split_sum = max(first_split_sum, solve(i + 1, scount - 1))
+                mlss = min(mlss, largest_split_sum)
 
-                # Find the maximum subarray sum for the current first split.
-                largest_split_sum = max(first_split_sum, 
-                                        get_min_largest_split_sum(i + 1, subarray_count - 1))
-
-                # Find the minimum among all possible combinations.
-                minimum_largest_split_sum = min(minimum_largest_split_sum, largest_split_sum)
-
-                if first_split_sum >= minimum_largest_split_sum:
+                if first_split_sum >= mlss:
                     break
             
-            return minimum_largest_split_sum
-        
-        return get_min_largest_split_sum(0, m)
+            return mlss
+            
+        return solve(0, m)
