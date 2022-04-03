893ff35f8490883254ffde08dcda4d5456e3e5da
Deepak Kumar <robinsdeepak@gmail.com>
2022-04-04 01:12:31 +0530

    Added solution - LeetHub

--- 4. Find the number of islands - GFG/4.-find-the-number-of-islands.py
+++ 4. Find the number of islands - GFG/4.-find-the-number-of-islands.py
@@ -6,27 +6,24 @@ class Solution:
     
     def numIslands(self, grid):
         m, n = len(grid), len(grid[0])
-        visited = [[False for i in range(n)] for j in range(m)]
-        
+
         def solve(x, y):
 
-            visited[x][y] = True
+            grid[x][y] = 0
 
             coords = [(x + i, y + j) for i in (-1, 0, 1) for j in (-1, 0, 1) if (i,j) != (0, 0)]
             
             for r, c in coords:
-                # print(r, c)
-                if 0 <= r < m and 0 <= c < n and grid[r][c] and not visited[r][c]:
+                if 0 <= r < m and 0 <= c < n and grid[r][c]:
                     solve(r, c)
             
         count = 0
         
         for i in range(m):
             for j in range(n):
-                if grid[i][j] and not visited[i][j]:
+                if grid[i][j]:
                     count += 1
                     solve(i, j)
-        # solve(0, 1)
         # print(visited)
         return count
     
