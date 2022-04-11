f2c9e4dc69165b476e546dd25c86bd29f1010f9c
Deepak Kumar <robinsdeepak@gmail.com>
2022-04-12 01:39:33 +0530

    Time: 339 ms (69.84%), Space: 15.8 MB (36.56%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -5,7 +5,6 @@ class Solution:
         v1 = [[0] * n for _ in range(m)]
         v2 = [[0] * n for _ in range(m)]
         
-        
         q1 = []
         q2 = []
         
@@ -18,8 +17,8 @@ class Solution:
             q2.append((m - 1, j))
         
         
-        def dfs1(x, y):
-            v1[x][y] = 1
+        def dfs(x, y, v):
+            v[x][y] = 1
             
             nbrs = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
 
@@ -27,29 +26,16 @@ class Solution:
                 if i < 0 or i >= m or j < 0 or j >= n:
                     continue
                 
-                if not v1[i][j] and heights[x][y] <= heights[i][j]:
-                    dfs1(i, j)
-        
-            
-        def dfs2(x, y):
-            v2[x][y] = 1
-            
-            nbrs = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-            
-            for i, j in nbrs:
-                if i < 0 or i >= m or j < 0 or j >= n:
-                    continue 
-                
-                if not v2[i][j] and heights[x][y] <= heights[i][j]:
-                    dfs2(i, j)
+                if not v[i][j] and heights[x][y] <= heights[i][j]:
+                    dfs(i, j, v)
         
         for x, y in q1:
             if not v1[x][y]:
-                dfs1(x, y)
+                dfs(x, y, v1)
         
         for x, y in q2:
             if not v2[x][y]:
-                dfs2(x, y)
+                dfs(x, y, v2)
         
         result = []
         
@@ -57,6 +43,5 @@ class Solution:
             for j in range(n):
                 if v1[i][j] and v2[i][j]:
                     result.append((i, j))
-        
         
         return result
