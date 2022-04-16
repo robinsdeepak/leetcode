558d075a6ff9107062c31ed902d443eaca381c3e
Deepak Kumar <robinsdeepak@gmail.com>
2022-04-16 17:46:03 +0530

    Time: 973 ms (36.45%), Space: 21.7 MB (54.24%) - LeetHub

---
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.py
@@ -0,0 +1,24 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        n = len(graph)
+        sn = set()
+        for i in range(n):
+            if len(graph[i]) == 0:
+                sn.add(i)
+
+        flag = True
+        
+        while flag:
+            flag = False           
+            for i in range(n):
+                if i in sn: continue
+                
+                for j in graph[i]:
+                    if j not in sn:
+                        break
+                else:
+                    sn.add(i)
+                    flag = True
+            if not flag:
+                break
+        return sorted(list(sn))
