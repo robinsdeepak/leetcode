8a2ca3c3994fbc4cf18aba3e911dcf46b07acc4d
Deepak Kumar <robinsdeepak@gmail.com>
2022-04-20 23:58:10 +0530

    Time: 1242 ms (17.87%), Space: 18.9 MB (93.57%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.py
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -1,7 +1,7 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trust_to = defaultdict(int)
-        trusted_by = defaultdict(int)
+        trust_to = {i : 0 for i in range(1, n + 1)}
+        trusted_by = {i : 0 for i in range(1, n + 1)}
         
         for i, j in trust:
             trust_to[i] += 1
@@ -11,4 +11,3 @@ class Solution:
             if trust_to[i] == 0 and trusted_by[i] == n - 1:
                 return i
         return -1
-
