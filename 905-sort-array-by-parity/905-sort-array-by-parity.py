5468b0853ea73484e236254186752267d3ef74de
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-02 09:30:02 +0530

    Time: 156 ms (11.37%), Space: 14.8 MB (17.91%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.py
+++ 905-sort-array-by-parity/905-sort-array-by-parity.py
@@ -1,3 +1,3 @@
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
-        return [n for n in nums if not n % 2] + [n for n in nums if n % 2]
+        return sorted(nums, key=lambda x:x % 2)
