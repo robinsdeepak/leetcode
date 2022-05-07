55650c41263d7208716156d5267aeef6c612f3e6
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-08 00:15:31 +0530

    Added solution - LeetHub

---
+++ 17. Steps by Knight - GFG/17.-steps-by-knight.py
@@ -0,0 +1,46 @@
+from functools import lru_cache
+
+class Solution:
+
+	#Function to find out minimum steps Knight needs to reach target position.
+	def minStepToReachTarget(self, KnightPos, TargetPos, N):
+        
+        q = [(*KnightPos, 0)]
+        
+        visited = {tuple(KnightPos)}
+
+        while len(q):
+            q2 = set()
+            for x, y, d in q:
+                if TargetPos == [x, y]:
+                    return d
+
+                dx = [2, 2, -2, -2, 1, 1, -1, -1]
+                dy = [1, -1, 1, -1, 2, -2, 2, -2]
+
+                for i in range(8):
+                    xx = x + dx[i]
+                    yy = y + dy[i]
+
+                    if (xx, yy) not in visited:
+                        visited.add((xx, yy))
+                        q2.add((xx, yy, d + 1))
+            q = q2
+
+        return -1
+
+
+
+#{ 
+#  Driver Code Starts
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		N = int(input())
+		KnightPos = list(map(int, input().split()))
+		TargetPos = list(map(int, input().split()))
+		obj = Solution()
+		ans = obj.minStepToReachTarget(KnightPos, TargetPos, N)
+		print(ans)
+
+# } Driver Code Ends
