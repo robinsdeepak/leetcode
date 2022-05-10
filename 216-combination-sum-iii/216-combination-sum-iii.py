b0f3da61c3541a36764d86820416cf522ef0a4a2
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-10 09:45:57 +0530

    Time: 54 ms (26.91%), Space: 13.9 MB (29.87%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -0,0 +1,21 @@
+class Solution:
+    
+    def __init__(self):
+        self.ans = []
+    
+    def backtrack(self, nums, k, n, path):
+        
+        if k == 0 and n == 0:
+            self.ans.append(path)
+        
+        if k < 0 or n < 0:
+            return
+        
+        for i in range(len(nums)):
+            self.backtrack(nums[i+1:], k - 1, n - nums[i], path + [nums[i]])
+            
+        
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        self.backtrack(list(range(1, 10)), k, n, [])
+        return self.ans
+
