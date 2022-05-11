bc5f0447ebb60c3a74eb0874f21c16eac9262608
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-11 09:21:21 +0530

    Time: 41 ms (59.59%), Space: 14 MB (14.87%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -5,12 +5,9 @@ class Solution:
         
         @lru_cache(maxsize=None)
         def rec(c, f):
-            if c == 4: return 1
-            if f == 1: return 5 - c
+            if c == 4 or f == 0: 
+                return 1
             
-            res = 0
-            for ff in range(f + 1):
-                res += rec(c + 1, f - ff)
-            return res
-        
+            return sum(rec(c + 1, f - ff) for ff in range(f + 1))
+                    
         return rec(0, n)
