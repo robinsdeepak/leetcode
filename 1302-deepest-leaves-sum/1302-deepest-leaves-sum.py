6e614d3f46175cc178057bf1fb6c0a9c038f2639
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-15 09:19:42 +0530

    Time: 292 ms (47.55%), Space: 17.8 MB (66.46%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -8,24 +8,21 @@ class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
-        
-        ans = 0
-        
+                
         q = [root]
         
         while len(q):
             q2 = set()
-            sm = 0
             
             for node in q:
-                sm += node.val
-                
                 if node.left:  q2.add(node.left)
                 if node.right: q2.add(node.right)
             
+            if not q2: 
+                return sum(map(lambda x: x.val, q))
+            
             q = q2
-            ans = sm
-        
-        return ans
+            
+        return -1
 
         
