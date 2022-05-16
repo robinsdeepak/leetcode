f5d64215a8621da296716e7d53e1f9336a05fcdb
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-16 09:16:39 +0530

    Time: 683 ms (76.60%), Space: 14.3 MB (63.63%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -5,23 +5,20 @@ class Solution:
         
         
         dirs = [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-        visited = {(0, 0)}
         q = [(0, 0, 1)]
         
         
         while len(q):
-            q2 = set()
+            q2 = []
             for x, y, d in q:
                 if x == y == n - 1:
                     return d
                 
                 for dx, dy in dirs:
                     x2, y2 = x + dx, y + dy
-                    if 0 <= x2 < n and 0 <= y2 < n and \
-                    (x2, y2) not in visited and grid[x2][y2] == 0:
-                        
-                        visited.add((x2, y2))
-                        q2.add((x2, y2, d + 1))
+                    if 0 <= x2 < n and 0 <= y2 < n and grid[x2][y2] == 0:
+                        grid[x2][y2] = 1                        
+                        q2.append((x2, y2, d + 1))
             
             q = q2
             
