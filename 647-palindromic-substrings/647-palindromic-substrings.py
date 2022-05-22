c5ace835be5fa5e35861f54a47585eea748a7ae9
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-23 02:15:53 +0530

    Time: 178 ms (55.71%), Space: 13.9 MB (75.86%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -5,27 +5,28 @@ class Solution:
     
     def solution_1(self, s):
         n = len(s)
-        q = [(i, i) for i in range(n)]
-        
-        for i in range(1, n):
-            if s[i - 1] == s[i]:
-                q.append((i - 1, i))
-        
         ans = 0
-        while len(q):
-            ans += len(q)
-            q2 = []
+        
+        for i in range(n):
+            l, h = i, i
             
-            for i, j in q:
-                i2, j2 = i - 1, j + 1
-                if i2 >= 0 and j2 < n and s[i2] == s[j2]:
-                    q2.append((i2, j2))
-            
-            q = q2
+            while l >= 0 and h < n and s[l] == s[h]:
+                ans += 1
+                l -= 1
+                h += 1
+        
+        for i in range(n - 1):
+            l, h = i, i + 1
+
+            while l >= 0 and h < n and s[l] == s[h]:
+                ans += 1
+                l -= 1
+                h += 1
         
         return ans
-                    
-        
+
+
+
     def solution_2(self, s):
         n = len(s)
         dp = [[0] * n for _ in range(n)]
