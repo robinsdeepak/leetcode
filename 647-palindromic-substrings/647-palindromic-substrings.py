768ec3a3510c3dbe92dc75ea54e54cdc36fe8327
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-23 02:18:33 +0530

    Time: 170 ms (59.04%), Space: 14 MB (29.30%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -2,32 +2,46 @@ class Solution:
     def countSubstrings(self, s: str) -> int:
         return self.solution_1(s)
         
-    
     def solution_1(self, s):
         n = len(s)
         ans = 0
         
         for i in range(n):
-            l, h = i, i
-            
-            while l >= 0 and h < n and s[l] == s[h]:
-                ans += 1
-                l -= 1
-                h += 1
-        
-        for i in range(n - 1):
-            l, h = i, i + 1
+            for j in range(2):
 
-            while l >= 0 and h < n and s[l] == s[h]:
-                ans += 1
-                l -= 1
-                h += 1
+                l, h = i, i + j
+
+                while l >= 0 and h < n and s[l] == s[h]:
+                    ans += 1
+                    l -= 1
+                    h += 1
+        return ans
+
+    
+    
+    def solution_2(self, s):
+        n = len(s)
+        q = [(i, i) for i in range(n)]
         
+        for i in range(1, n):
+            if s[i - 1] == s[i]:
+                q.append((i - 1, i))
+        
+        ans = 0
+        while len(q):
+            ans += len(q)
+            q2 = []
+            
+            for i, j in q:
+                i2, j2 = i - 1, j + 1
+                if i2 >= 0 and j2 < n and s[i2] == s[j2]:
+                    q2.append((i2, j2))
+            q = q2
+            
         return ans
 
 
-
-    def solution_2(self, s):
+    def solution_3(self, s):
         n = len(s)
         dp = [[0] * n for _ in range(n)]
         ans = n
