2b76f4568d53e4025cf2c4f50a751feade3c83b6
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-22 17:16:14 +0530

    Time: 270 ms (35.36%), Space: 14.5 MB (26.85%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.py
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -1,7 +1,32 @@
-from functools import lru_cache
-
 class Solution:
     def countSubstrings(self, s: str) -> int:
+        return self.solution_1(s)
+        
+    
+    def solution_1(self, s):
+        n = len(s)
+        q = [(i, i) for i in range(n)]
+        
+        for i in range(1, n):
+            if s[i - 1] == s[i]:
+                q.append((i - 1, i))
+        
+        ans = 0
+        while len(q):
+            ans += len(q)
+            q2 = []
+            
+            for i, j in q:
+                i2, j2 = i - 1, j + 1
+                if i2 >= 0 and j2 < n and s[i2] == s[j2]:
+                    q2.append((i2, j2))
+            
+            q = q2
+        
+        return ans
+                    
+        
+    def solution_2(self, s):
         n = len(s)
         dp = [[0] * n for _ in range(n)]
         ans = n
@@ -20,7 +45,5 @@ class Solution:
                 if s[i] == s[j] and dp[i + 1][j - 1]:
                     dp[i][j] = 1
                     ans += 1
-                      
+                
         return ans
-
-        
