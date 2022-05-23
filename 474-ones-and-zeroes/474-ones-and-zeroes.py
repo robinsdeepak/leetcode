1eed90a1ca4fd1dc5ca78bb587ecaa12eebd4038
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-23 23:51:12 +0530

    Time: 5157 ms (28.25%), Space: 14.1 MB (81.21%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -3,6 +3,22 @@ from functools import lru_cache
 class Solution:
 
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        return self.iterative(strs, m, n)
+    
+    def iterative(self, strs, m, n):
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for s in strs:
+            c0 = s.count("0")
+            c1 = len(s) - c0
+            
+            for i in range(m, c0 - 1, -1):
+                for j in range(n, c1 - 1, -1):
+                    dp[i][j] = max(dp[i][j], dp[i - c0][j - c1] + 1)
+            
+        return dp[m][n]
+    
+    def recursive(self, strs, m, n):
         ln = len(strs)
         inf = float('inf')
         
@@ -16,5 +32,4 @@ class Solution:
             
             return max(rec(i + 1, c0, c1), rec(i + 1, c0 - cc0, c1 - cc1) + 1)
             
-        
         return rec(0, m, n)
