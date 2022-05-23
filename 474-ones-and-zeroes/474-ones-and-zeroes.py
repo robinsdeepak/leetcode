0c6fb733e7c307f953da887931e917c1c41ab34e
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-23 23:16:48 +0530

    Time: 3245 ms (75.66%), Space: 307.3 MB (5.05%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.py
+++ 474-ones-and-zeroes/474-ones-and-zeroes.py
@@ -8,16 +8,13 @@ class Solution:
         
         @lru_cache(maxsize=None)
         def rec(i, c0, c1):
-            if i == ln:
-                return 0
+            if c0 < 0 or c1 < 0: return -inf
+            if i == ln: return 0
             
             cc0 = strs[i].count("0")
             cc1 = len(strs[i]) - cc0
             
-            curr = rec(i + 1, c0, c1)
-            if cc0 <= c0 and cc1 <= c1:
-                curr = max(curr, rec(i + 1, c0 - cc0, c1 - cc1) + 1)
+            return max(rec(i + 1, c0, c1), rec(i + 1, c0 - cc0, c1 - cc1) + 1)
             
-            return curr
         
         return rec(0, m, n)
