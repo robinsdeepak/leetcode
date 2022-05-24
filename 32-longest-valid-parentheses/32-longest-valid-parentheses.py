553fd86bb77773e8b1528b0334a84ae3824a283a
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-24 07:26:19 +0530

    Time: 73 ms (32.46%), Space: 14.7 MB (46.94%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -3,8 +3,7 @@ class Solution:
         st = []
         ans = 0
         for i in range(len(s)):
-            ch = s[i]
-            if st and s[st[-1]] == '(' and ch == ')':
+            if st and s[st[-1]] == '(' and s[i] == ')':
                 st.pop()
                 curr = i - st[-1] if st else i + 1
                 ans = max(ans, curr)
