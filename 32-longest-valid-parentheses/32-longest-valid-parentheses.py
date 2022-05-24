46e042e522df7b4f98a9f0bc70124be3d85f055a
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-24 07:44:54 +0530

    Time: 68 ms (39.74%), Space: 14.1 MB (91.28%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.py
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.py
@@ -1,12 +1,22 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        st = []
-        ans = 0
-        for i in range(len(s)):
-            if st and s[st[-1]] == '(' and s[i] == ')':
-                st.pop()
-                curr = i - st[-1] if st else i + 1
-                ans = max(ans, curr)
-            else:
-                st.append(i)
+        left, right, ans = 0, 0, 0
+        
+        loops = [(0, len(s), 1), (len(s) - 1, -1, -1)]
+        
+        for start, end, step in loops:
+            left, right = 0, 0
+            for i in range(start, end, step):
+                if s[i] == '(':
+                    left += 1
+                else:
+                    right += 1
+
+                if left == right:
+                    ans = max(ans, 2 * right)
+
+                if (step == 1 and right > left) or (step == -1 and left > right):
+                    left, right = 0, 0
+            
+        
         return ans
