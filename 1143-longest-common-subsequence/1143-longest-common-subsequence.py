8319caab867e6d0c6090280c6a2e5ea29d476f4e
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-27 00:11:56 +0530

    Time: 840 ms (29.20%), Space: 22 MB (76.71%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -5,7 +5,25 @@ class Solution:
         self.s1 = s1
         self.s2 = s2
         
-        return self.rec(len(s1) - 1, len(s2) - 1)
+        # return self.rec(len(s1) - 1, len(s2) - 1)
+        return self.tab()
+    
+    def tab(self):
+        m = len(self.s1)
+        n = len(self.s2)
+        
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if self.s1[i - 1] == self.s2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        # for r in dp: print(r)
+            
+        return dp[m][n]
         
     @lru_cache(maxsize=None)
     def rec(self, i1, i2):
