f314eb9f59b6325da5830c30c7ddc5a1278e010f
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-26 23:44:23 +0530

    Time: 2118 ms (5.01%), Space: 142.4 MB (6.94%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -2,15 +2,18 @@ from functools import lru_cache
 
 class Solution:
     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
+        self.s1 = s1
+        self.s2 = s2
         
-        @lru_cache(maxsize=None)
-        def rec(i1, i2):
-            if i1 < 0 or i2 < 0:
-                return 0
-            
-            if s1[i1] == s2[i2]:
-                return 1 + rec(i1 - 1, i2 - 1)
-            
-            return max(rec(i1 - 1, i2), rec(i1, i2 - 1))
+        return self.rec(len(s1) - 1, len(s2) - 1)
         
-        return rec(len(s1) - 1, len(s2) - 1)
+    @lru_cache(maxsize=None)
+    def rec(self, i1, i2):
+        if i1 < 0 or i2 < 0:
+            return 0
+
+        if self.s1[i1] == self.s2[i2]:
+            return 1 + self.rec(i1 - 1, i2 - 1)
+
+        return max(self.rec(i1 - 1, i2), self.rec(i1, i2 - 1))
+
