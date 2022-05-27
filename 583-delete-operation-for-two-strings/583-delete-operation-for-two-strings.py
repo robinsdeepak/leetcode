85399e6fbbd0c04cd58f94d3c0d6ada8b0ab0138
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-27 10:01:35 +0530

    Time: 298 ms (77.84%), Space: 17.7 MB (44.89%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -3,14 +3,15 @@ class Solution:
         n1, n2 = len(w1), len(w2)
         memo = [[0] * (n2 + 1) for _ in range(n1 + 1)]
         
-        for i in range(1, n1 + 1):
-            for j in range(1, n2 + 1):
-                if w1[i - 1] == w2[j - 1]:
-                    memo[i][j] = memo[i - 1][j - 1] + 1
+        for i in range(n1 + 1):
+            for j in range(n2 + 1):
+                if (i == 0 or j == 0):
+                    memo[i][j] = i + j
+                    
+                elif w1[i - 1] == w2[j - 1]:
+                    memo[i][j] = memo[i - 1][j - 1]
+                    
                 else:
-                    memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
+                    memo[i][j] = min(memo[i - 1][j], memo[i][j - 1]) + 1
         
-#         for r in memo:
-#             print(r)
-        
-        return n1 + n2 - memo[n1][n2] * 2
+        return memo[n1][n2]
