5076f183b00db6fed2306bc8cb4196f6ec70523d
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-29 10:49:15 +0530

    Time: 835 ms (62.98%), Space: 14.5 MB (29.91%) - LeetHub

---
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -0,0 +1,24 @@
+class Solution:
+    
+    def maxProduct(self, words: List[str]) -> int:
+        n = len(words)
+        
+        def bits(w):
+            x = 0
+            for c in w:
+                x |= (1 << (ord(c) - 97))
+            return x
+        
+        freq = []
+        for w in words:
+            freq.append((bits(w), len(w)))
+        
+        freq.sort(key=lambda x: -x[1])
+        
+        ans = 0
+        for i in range(n):
+            for j in range(i + 1, n):
+                if not (freq[i][0] & freq[j][0]):
+                    ans = max(ans, (freq[i][1] * freq[j][1]))
+        
+        return ans
