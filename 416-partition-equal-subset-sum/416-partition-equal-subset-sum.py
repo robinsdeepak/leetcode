12e718eb02bc2f3435994201e64f2d3945e74f1a
Deepak Kumar <robinsdeepak@gmail.com>
2022-05-29 12:57:50 +0530

    Time: 73 ms (99.21%), Space: 14.1 MB (87.40%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -3,6 +3,26 @@ from functools import lru_cache
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        return self.solution_3(nums)
+    
+    def solution_2(self, nums):
+        # tabulation
+        sm = sum(nums)
+        half = sm // 2
+        
+        
+    def solution_3(self, nums):
+        total_sum = sum(nums)
+        if total_sum & 1: return False
+        half_sum, dp = total_sum // 2, 1
+        for num in nums:
+            dp |= dp << num
+        return dp & 1 << half_sum
+        
+        
+        
+    def solution_1(self, nums):
+        # memoization
         sm = sum(nums)
         if sm & 1: return False
         
