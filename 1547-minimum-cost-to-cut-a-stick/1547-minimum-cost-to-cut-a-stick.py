bdf0db5f544ae58e02e88bae4bfa4a148b2d5156
Deepak Kumar <robinsdeepak@gmail.com>
2022-06-01 23:51:06 +0530

    Time: 1597 ms (55.24%), Space: 14.4 MB (70.40%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -4,14 +4,36 @@ class Solution:
     
     
     def minCost(self, n: int, cuts: List[int]) -> int:
-        cuts.sort()
-        cuts = [0] + cuts + [n]
+        return self.solution_2(n, cuts)
+
+    def solution_2(self, n, cuts):
+        c = len(cuts)
+        cuts = [0] + sorted(cuts) + [n]
         
+        dp = [[0] * (c + 2) for _ in range(c + 2)]
+        
+        for i in range(c, 0, -1):
+            for j in range(1, c + 1):
+                if i > j: continue
+                    
+                mini = float('inf')
+
+                for x in range(i, j + 1):
+                    curr = dp[i][x - 1] + dp[x + 1][j] + cuts[j + 1] - cuts[i - 1]
+
+                    mini = min(curr, mini)
+
+                dp[i][j] = mini
+                    
+        return dp[1][c]
+        
+    def solution_1(self, n, cuts):
+        c = len(cuts)
+        cuts = [0] + sorted(cuts) + [n]
         
         @lru_cache(maxsize=None)
-        def solve(i, j):            
-            if i > j:
-                return 0
+        def solve(i, j):
+            if i > j: return 0
             
             ans = float('inf')
             
@@ -22,8 +44,6 @@ class Solution:
                 
                 ans = min(curr, ans)
             
-            
             return ans
         
-        return solve(1, len(cuts) - 2)
-
+        return solve(1, c - 2)
