381422131f9580ec5d1c382c58bb993f0fbfeb17
Deepak Kumar <robinsdeepak@gmail.com>
2022-06-08 08:45:01 +0530

    Time: 1678 ms (70.40%), Space: 251.2 MB (5.61%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -1,21 +1,41 @@
+from functools import lru_cache
+import sys
+sys.setrecursionlimit(10**6)
+
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        return self.lcs(s, s[::-1])
+        return self.solution_2(s)
+    
+    def solution_2(self, s):
+        # recursive and memoization
         
+        @lru_cache(maxsize=None)
+        def rec(i, j):
+            if i == j: return 1
+            if i > j: return 0
+
+            if s[i] == s[j]:
+                return 2 + rec(i + 1, j - 1)
+            else:
+                return max(rec(i + 1, j), rec(i, j - 1))
+            
+        return rec(0, len(s) - 1)
+    
+    def solution_1(self, s):
+        # lcs and dp
+        return self.lcs(s, s[::-1])
+    
     def lcs(self, w1, w2):
         n1, n2 = len(w1), len(w2)
-        # memo = [[0] * (n2 + 1) for _ in range(n1 + 1)]
         dp = [0] * (n2 + 1)
         
         for i in range(1, n1 + 1):
             temp = [0] * (n2 + 1)
             for j in range(1, n2 + 1):
                 if w1[i - 1] == w2[j - 1]:
-                    # memo[i][j] = memo[i - 1][j - 1] + 1
                     temp[j] = dp[j - 1] + 1
                 else:
-                    # memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
                     temp[j] = max(dp[j], temp[j - 1])
             dp = temp
-                
+            
         return dp[n2]
