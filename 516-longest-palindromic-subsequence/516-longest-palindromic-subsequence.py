20783dc90ee405e15d619d0c968bf280e48a903a
Deepak Kumar <robinsdeepak@gmail.com>
2022-06-09 01:17:18 +0530

    Time: 1068 ms (92.60%), Space: 14.1 MB (92.99%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -4,7 +4,7 @@ sys.setrecursionlimit(10**6)
 
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        return self.solution_3(s)
+        return self.solution_4(s)
     
     def solution_3(self, s):
         # dp
@@ -23,6 +23,25 @@ class Solution:
         
         return dp[0][n - 1]
 
+    def solution_4(self, s):
+        # dp + space optimization
+        n = len(s)
+
+        dp = [0] * n
+        dp[n - 1] = 1
+        
+        for i in range(n - 2, -1, -1):
+            temp = [0] * n
+            temp[i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    temp[j] = 2 + dp[j - 1]
+                else:
+                    temp[j] = max(dp[j], temp[j - 1])
+            dp = temp
+        
+        return dp[n - 1]
+    
     
     def solution_2(self, s):
         # recursive and memoization
