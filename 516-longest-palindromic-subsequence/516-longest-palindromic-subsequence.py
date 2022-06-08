35dbbc41004dbf09ccd1b677fea5a24c4f9f5180
Deepak Kumar <robinsdeepak@gmail.com>
2022-06-09 01:19:14 +0530

    Time: 1987 ms (58.82%), Space: 14.1 MB (92.99%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.py
@@ -5,6 +5,39 @@ sys.setrecursionlimit(10**6)
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         return self.solution_4(s)
+
+    def solution_1(self, s):
+        # lcs and dp
+        w1, w2 = s, s[::-1]
+        
+        n1, n2 = len(w1), len(w2)
+        dp = [0] * (n2 + 1)
+        
+        for i in range(1, n1 + 1):
+            temp = [0] * (n2 + 1)
+            for j in range(1, n2 + 1):
+                if w1[i - 1] == w2[j - 1]:
+                    temp[j] = dp[j - 1] + 1
+                else:
+                    temp[j] = max(dp[j], temp[j - 1])
+            dp = temp
+            
+        return dp[n2]
+
+    def solution_2(self, s):
+        # recursive and memoization
+        
+        @lru_cache(maxsize=None)
+        def rec(i, j):
+            if i == j: return 1
+            if i > j: return 0
+
+            if s[i] == s[j]:
+                return 2 + rec(i + 1, j - 1)
+            else:
+                return max(rec(i + 1, j), rec(i, j - 1))
+            
+        return rec(0, len(s) - 1)
     
     def solution_3(self, s):
         # dp
@@ -43,36 +76,3 @@ class Solution:
         return dp[n - 1]
     
     
-    def solution_2(self, s):
-        # recursive and memoization
-        
-        @lru_cache(maxsize=None)
-        def rec(i, j):
-            if i == j: return 1
-            if i > j: return 0
-
-            if s[i] == s[j]:
-                return 2 + rec(i + 1, j - 1)
-            else:
-                return max(rec(i + 1, j), rec(i, j - 1))
-            
-        return rec(0, len(s) - 1)
-    
-    
-    def solution_1(self, s):
-        # lcs and dp
-        w1, w2 = s, s[::-1]
-        
-        n1, n2 = len(w1), len(w2)
-        dp = [0] * (n2 + 1)
-        
-        for i in range(1, n1 + 1):
-            temp = [0] * (n2 + 1)
-            for j in range(1, n2 + 1):
-                if w1[i - 1] == w2[j - 1]:
-                    temp[j] = dp[j - 1] + 1
-                else:
-                    temp[j] = max(dp[j], temp[j - 1])
-            dp = temp
-            
-        return dp[n2]
