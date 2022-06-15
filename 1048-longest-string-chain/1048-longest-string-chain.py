4314f3cf1c525beff46c9c175d7cc3a245198d2f
Deepak Kumar <robinsdeepak@gmail.com>
2022-06-15 20:25:33 +0530

    Time: 214 ms (62.94%), Space: 16.9 MB (12.71%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,18 +1,18 @@
-from functools import lru_cache
-
-    
+  
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
         
         words = set(words)
+        dp = {}
         
-        @lru_cache(None)
         def rec(w):
+            if dp.get(w): return dp[w]
             ans = 1
             for i in range(len(w)):
                 w2 = w[:i] + w[i+1:]
                 if w2 in words:
                     ans = max(ans, rec(w2) + 1)
+            dp[w] = ans
             return ans
                 
         return max(map(rec, words))
