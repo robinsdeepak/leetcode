595f19c4d14b3bf8cc54b15f9765149143547d63
Deepak Kumar <robinsdeepak@gmail.com>
2022-06-15 20:20:04 +0530

    Time: 5354 ms (5.01%), Space: 70.7 MB (5.76%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,17 +1,37 @@
 from functools import lru_cache
 
+
+def isPredecessor(w1, w2):
+    if len(w1) != len(w2) - 1:
+        return False
+    
+    for i in range(len(w2)):
+        w3 = w2[:i] + w2[i+1:]
+        if w3 == w1:
+            return True
+        
+    return False
+    
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
         
-        words = set(words)
+        n = len(words)
+        
+        mat = [[0] * n for _ in range(n)]
+        
+        words.sort(key=lambda x: (len(x), x))
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                if isPredecessor(words[i], words[j]):
+                    mat[i][j] = 1
         
         @lru_cache(None)
-        def dp(w):
+        def rec(i):
             ans = 1
-            for i in range(len(w)):
-                pw = w[:i] + w[i+1:]
-                if pw in words:
-                    ans = max(ans, dp(pw) + 1)
+            for j in range(i):
+                if mat[j][i]:
+                    ans = max(ans, rec(j) + 1)
             return ans
         
-        return max((dp(w) for w in words))
+        return max((rec(i) for i in range(n)))
