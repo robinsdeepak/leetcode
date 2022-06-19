f1f19c504c7e27a9c38997aea48cd7cb7a06371a
Deepak Kumar <robinsdeepak@gmail.com>
2022-06-19 15:07:52 +0530

    Time: 81 ms (96.17%), Space: 17 MB (98.45%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,19 +1,14 @@
+import bisect
+
 class Solution:
     def suggestedProducts(self, products: List[str], sw: str) -> List[List[str]]:
+        products.sort()
         
-        ans = [[] for _ in sw]
+        ans, prefix, i = [], '', 0
         
-        for pd in products:
-            for j in range(min(len(pd), len(sw))):
-                if pd[j] != sw[j]:
-                    break
-                
-                if len(ans[j]) < 3:
-                    ans[j].append(pd)
-                    
-                elif pd < ans[j][2]:
-                    ans[j][2] = pd
-                    
-                ans[j].sort()
-                
+        for c in sw:
+            prefix += c
+            i = bisect.bisect_left(products, prefix, i)
+            ans.append([w for w in products[i:i+3] if w.startswith(prefix)])
+            
         return ans
