1e4cf32b4986a490e9a8972cf715545533ab9cba
Deepak Kumar <robinsdeepak@gmail.com>
2022-06-27 09:31:58 +0530

    Time: 39 ms (99.41%), Space: 14.7 MB (50.25%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -1,5 +1,11 @@
 class Solution:
     def minPartitions(self, n: str) -> int:
+        return self.solution_3(n)
+    
+    def solution_1(self, n):
+        return int(max(n))
+        
+    def solution_2(self, n):
         ans = 1
         for c in n:
             if c == '9':
@@ -7,4 +13,10 @@ class Solution:
             if int(c) > ans:
                 ans = int(c)
         return ans
-
+    
+    def solution_3(self, n):
+        s = '987654321'
+        for c in s:
+            if c in n:
+                return int(c)
+        
