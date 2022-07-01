5c231f96f9de9acf912b166b24349d5fe28c561a
Deepak Kumar <robinsdeepak@gmail.com>
2022-07-01 06:41:09 +0530

    Time: 86 ms (83.90%), Space: 15.4 MB (79.38%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -1,26 +1,16 @@
 class Solution:
     def minMoves2(self, nums: List[int]) -> int:
-        def cost(x, s, e):
-            return sum(map(lambda y: abs(x - nums[y]), range(s, e + 1)))
-        
+        total = sum(nums)
+        n = len(nums)
+        curr = 0
+        ans = float('inf')
         nums.sort()
         
-        n = len(nums)
-        l, h = 0, n - 1
-        
-        ans = float('inf')
-        
-        while l <= h:
-            m = (l + h) // 2
-            c1 = cost(nums[m], 0, m)
-            c2 = cost(nums[m], m + 1, n - 1)
-            
+        for i in range(n):
+            curr += nums[i]
+            c1 = nums[i] * (i + 1) - curr
+            c2 = (total - curr) - nums[i] * (n - i - 1)
+            # print(ans, c1, c2)
             ans = min(ans, c1 + c2)
-            
-            if c1 < c2:
-                l = m + 1
-            else:
-                h = m - 1
-            
+        
         return ans
-
