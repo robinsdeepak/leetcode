b728a4d3a9cfa0a3bec91f5d72171db09bfb9415
Deepak Kumar <robinsdeepak@gmail.com>
2022-07-02 08:59:16 +0530

    Time: 387 ms (71.79%), Space: 26.7 MB (94.22%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.py
@@ -5,26 +5,13 @@ class Solution:
         vct.sort()
         mod = 10 ** 9 + 7
         
-        if hct[0] != 0:
-            hct.insert(0, 0)
-        
-        if hct[-1] != w:
-            hct.append(h)
-            
-        if vct[0] != 0:
-            vct.insert(0, 0)
-        
-        if vct[-1] != w:
-            vct.append(w)
-        
-        mh = 1
+        mh = max(hct[0], h - hct[-1])
+        mv = max(vct[0], w - vct[-1])
         
         for i in range(1, len(hct)):
             mh = max(mh, hct[i] - hct[i - 1])
         
-        
-        mv = 1
-        
+                
         for i in range(1, len(vct)):
             mv = max(mv, vct[i] - vct[i - 1])
             
