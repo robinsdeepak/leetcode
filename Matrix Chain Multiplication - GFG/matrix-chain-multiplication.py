18745ad2786bee39b3df4a4e64ebed536d60af62
Deepak Kumar <robinsdeepak@gmail.com>
2022-07-10 23:28:03 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
@@ -3,7 +3,7 @@ from functools import lru_cache
 
 class Solution:
     def matrixMultiplication(self, n, arr):
-        return self.solution_1(n, arr)
+        return self.solution_2(n, arr)
         
     def solution_1(self, n, arr):
         @lru_cache(None)
@@ -24,7 +24,24 @@ class Solution:
             return ans
             
         return solve(1, n - 1)
+    
+    
+    def solution_2(self, n, arr):
+        dp = [[0] * n for _ in range(n)]
+        inf = float('inf')
+        for i in range(n - 1, 0, -1):
+            for j in range(i + 1, n):
+                dp[i][j] = inf
+                for m in range(i, j):
+                    l = dp[i][m]
+                    r = dp[m + 1][j]
+                    mc = arr[i - 1] * arr[m] * arr[j] # merge cost
+                    
+                    curr = l + r + mc
+                    
+                    dp[i][j] = min(curr, dp[i][j])
         
+        return dp[1][n - 1]            
 
 #{ 
 #  Driver Code Starts
