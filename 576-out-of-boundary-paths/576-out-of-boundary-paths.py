1779196b74a89bdcc22036af3aedb6402306ef14
Deepak Kumar <robinsdeepak@gmail.com>
2022-07-19 22:43:54 +0530

    Time: 100 ms (94.88%), Space: 20.1 MB (26.60%) - LeetHub

---
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+
+MOD = 10 ** 9 + 7
+
+class Solution:
+    def findPaths(self, m, n, maxMove, startRow, startColumn):
+        
+        
+        @lru_cache(None)
+        def rec(sr, sc, mm):
+            if sr < 0 or sr >= m or sc < 0 or sc >= n:
+                return 1
+            
+            if mm == 0: return 0
+            
+            return  (
+                rec(sr + 1, sc, mm - 1) + 
+                rec(sr, sc + 1, mm - 1) + 
+                rec(sr - 1, sc, mm - 1) + 
+                rec(sr, sc - 1, mm - 1)
+            ) % MOD
+        
+        return rec(startRow, startColumn, maxMove)
