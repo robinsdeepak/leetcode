839f6f9b6391ad9358c3161acc71209cbe62d321
Deepak Kumar <robinsdeepak@gmail.com>
2022-07-19 22:20:22 +0530

    Time: 24 ms (99.20%), Space: 13.8 MB (66.48%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+
+    
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = [[1]]
+        for i in range(1, numRows):
+            triangle.append([1]*(i+1))
+            
+            for j in range(1, i):
+                triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+            
+        return triangle
