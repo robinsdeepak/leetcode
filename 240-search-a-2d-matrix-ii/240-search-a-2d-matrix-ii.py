c25f5c0318d08728a5c0e7c397394787f87902a7
Deepak Kumar <robinsdeepak@gmail.com>
2022-07-24 17:43:28 +0530

    Time: 188 ms (84.94%), Space: 20.4 MB (84.45%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.py
@@ -2,13 +2,31 @@ from bisect import bisect_left
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        m = len(matrix)
-        n = len(matrix[0])
+        return self.solution_2(matrix, target)
+    
+    def solution_1(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
         
         for r in range(m):
             idx = bisect_left(matrix[r], target)
             if idx < n and matrix[r][idx] == target:
                 return True
+        
+        return False
+
+    
+    def solution_2(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
+        
+        r, c = m - 1, 0
+        
+        
+        while r >= 0 and c < n:
+            if matrix[r][c] == target:
+                return True
+            elif matrix[r][c] > target:
+                r -= 1
+            else:
+                c += 1
         
         return False
