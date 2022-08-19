b74564b856cc4d4ab307bbb37e1acfbd8bc49db5
Deepak Kumar <robinsdeepak@gmail.com>
2022-08-19 23:53:51 +0530

    Time: 941 ms (35.20%), Space: 15.4 MB (25.22%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.py
@@ -1,21 +1,25 @@
 class Solution:
     def isPossible(self, nums: List[int]) -> bool:
-        f = collections.Counter(nums)
+        seq = defaultdict(int)
+        freq = collections.Counter(nums)
         
-        s = []
-        
-        for x in nums:          
-            for i in range(len(s) - 1, -1, -1):
-                if s[i][0] == x - 1:
-                    s[i][0] += 1
-                    s[i][1] += 1
-                    break
+        for n in nums:
+            if not freq[n]:
+                continue
+            
+            if seq[n - 1] > 0:
+                seq[n - 1] -= 1
+                seq[n] += 1
+                freq[n] -= 1
+            
             else:
-                s.append([x, 1])
-                # s.sort(key=lambda e: e[1])
-        
-        for n, c in s:
-            if c < 3:
-                return False
+                if not freq[n + 1] or not freq[n + 2]:
+                    return False
+                
+                freq[n] -= 1
+                freq[n + 1] -= 1
+                freq[n + 2] -= 1
+                seq[n + 2] += 1
+            
         
         return True
