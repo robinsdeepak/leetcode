3c5160cd45a6c80b15bd1d8f531f9292ec08244c
Deepak Kumar <robinsdeepak@gmail.com>
2022-08-24 07:53:25 +0530

    Time: 108 ms (74.46%), Space: 13.9 MB (57.97%) - LeetHub

--- 326-power-of-three/326-power-of-three.py
+++ 326-power-of-three/326-power-of-three.py
@@ -1,8 +1,19 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
+        return self.solution_2(n)
+    
+    def solution_1(self, n):
         while n > 1:
             if n % 3:
                 return False
             n //= 3
         
         return n == 1
+    
+    def solution_2(self, n):
+        p3 = {
+            1, 3, 9, 27, 81, 243, 729, 2187, 6561, 19683, 59049, 177147,
+            531441, 1594323, 4782969, 14348907, 43046721, 129140163, 
+            387420489, 1162261467, 3486784401
+        }
+        return n in p3
