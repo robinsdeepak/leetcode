7d7705e66d856910ff9aada8bcfb367b0d7cf8b9
Deepak Kumar <robinsdeepak@gmail.com>
2022-09-01 22:17:12 +0530

    Time: 576 ms (5.03%), Space: 32.7 MB (7.60%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -4,9 +4,13 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        
+        return self.solution_2(root)
+    
+    def solution_1(self, root):
         def preorder(node, mx):
             if not node:
                 return 0
@@ -16,3 +20,25 @@ class Solution:
             return preorder(node.left, curr_max) + preorder(node.right, curr_max) + (mx <= node.val)
         
         return preorder(root, -inf)
+
+    def solution_2(self, root):
+        q = [(root, -inf)]
+        
+        ans = 0
+        
+        while len(q):
+            q2 = []
+            
+            for node, mx in q:
+                if node.val >= mx:
+                    ans += 1
+                
+                if node.left:
+                    q.append((node.left,  max(mx, node.val)))
+                if node.right:
+                    q.append((node.right, max(mx, node.val)))
+            
+            q = q2
+        
+        return ans
+
