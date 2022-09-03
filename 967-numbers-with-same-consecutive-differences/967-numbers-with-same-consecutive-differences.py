58f2a035a832866f8d41afd2a2ffd98587616356
Deepak Kumar <robinsdeepak@gmail.com>
2022-09-03 10:39:03 +0530

    Time: 34 ms (98.64%), Space: 14.2 MB (41.16%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -1,5 +1,8 @@
 class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        return self.solution_2(n, k)
+        
+    def solution_1(self, n, k):
         self.ans = []
         
         def rec(num, l, c):
@@ -21,3 +24,22 @@ class Solution:
             rec(i, i, 1)
         
         return self.ans
+    
+    def solution_2(self, n, k):
+        if k == 0:
+            return list(map(lambda x: int(str(x) * n), range(1, 10)))
+        
+        numbers = list(range(1, 10))
+        
+        for _ in range(n - 1):
+            numbers2 = []
+            for x in numbers:
+                l = x % 10
+                if l + k < 10:
+                    numbers2.append(x * 10 + (l + k))
+                if l - k >= 0:
+                    numbers2.append(x * 10 + (l - k))
+            numbers = numbers2
+        
+        return numbers
+
