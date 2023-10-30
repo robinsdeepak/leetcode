653856e798f3a5ade00f5dbca0792743167a857a
Deepak Kumar <robinsdeepak@gmail.com>
2023-10-30 19:28:42 +0530

    Time: 78 ms (27.89%), Space: 16.4 MB (75.72%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,8 +1,22 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
-        def bitcount(n):
-            return n.bit_count(), n
+        return self.solution_2(arr)
+    
+    def solution_1(self, arr):
         
-        arr.sort(key=bitcount)
+        arr.sort(key=lambda x: (n.bit_count(), n))
         
+        return arr
+    
+    def solution_2(self, arr):
+        def find_weight(num):
+            weight = 0
+            
+            while num:
+                weight += 1
+                num &= (num - 1)
+            
+            return weight
+        
+        arr.sort(key = lambda num: (find_weight(num), num))
         return arr
