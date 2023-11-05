a8ed2bed9998ecbe8f28e9d726eadae3ed743c5c
Deepak Kumar <robinsdeepak@gmail.com>
2023-11-05 22:00:21 +0530

    Time: 826 ms (55.89%), Space: 30.4 MB (60.66%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.py
+++ 0896-monotonic-array/0896-monotonic-array.py
@@ -2,8 +2,16 @@ class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         diff = 0
         n = len(nums)
+        
+        def get_diff_sign(a, b):
+            if a < b:
+                return -1
+            if a > b:
+                return 1
+            return 0
+        
         for i in range(1, n):
-            curr_diff = nums[i] - nums[i - 1]
+            curr_diff = get_diff_sign(nums[i], nums[i - 1])
             if diff * curr_diff < 0:
                 return False
             if diff == 0 and curr_diff != 0:
