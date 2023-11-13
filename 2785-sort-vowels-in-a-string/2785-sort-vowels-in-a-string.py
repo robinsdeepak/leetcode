e81a8f4806431f253a283e516df32083fdaf5f5c
Deepak Kumar <robinsdeepak@gmail.com>
2023-11-13 16:54:03 +0530

    Time: 152 ms (54.91%), Space: 18.3 MB (95.53%) - LeetHub

---
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortVowels(self, s: str) -> str:
+        ascii_vowels = 'AEIOUaeiou'
+        m = {c: i for i, c in enumerate(ascii_vowels)}
+        freq = [0] * len(ascii_vowels)
+        # print(m)
+        for c in s:
+            if c in m:
+                freq[m[c]] += 1
+        # print(freq)
+        chars = list(s)
+        ptr = 0
+        for i, c in enumerate(chars):
+            if c not in m:
+                continue
+            
+            while ptr < 10 and freq[ptr] == 0:
+                ptr += 1
+            
+            if ptr > 9: 
+                break
+
+            freq[ptr] -= 1
+            chars[i] = ascii_vowels[ptr]
+            # print(i, c, ptr, chars)
+        return "".join(chars)
