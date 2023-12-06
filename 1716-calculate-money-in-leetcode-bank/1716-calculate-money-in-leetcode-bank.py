70adf0ff75dbf1f6f7501336e218dffb16bbcff6
Deepak Kumar <robinsdeepak@gmail.com>
2023-12-06 13:53:50 +0530

    Time: 32 ms (93.96%), Space: 16.1 MB (73.58%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -2,4 +2,5 @@ class Solution:
     def totalMoney(self, n: int) -> int:
         d, r = n // 7, n % 7
         
-        return d * 28 + 7 * (d - 1) * d // 2 + r * (d + 1 + d + r) // 2
+        return d * 21 + 7 * d * (d + 1) // 2 + \
+               d * r  +     r * (r + 1) // 2
